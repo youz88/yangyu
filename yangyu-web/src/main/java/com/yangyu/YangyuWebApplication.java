4966a63b128dc1f61af160603ec9f2d7851af4ea
youz <youz@wolianw.com>
2017-11-14 18:06:26 +0800

    新增资讯模块

---
+++ yangyu-web/src/main/java/com/yangyu/YangyuWebApplication.java
@@ -0,0 +1,34 @@
+package com.yangyu;
+
+import com.yangyu.common.util.A;
+import com.yangyu.common.util.LogUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableFeignClients(basePackages = {"com.yangyu.api"})
+public class YangyuWebApplication {
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(YangyuWebApplication.class, args);
+		if (LogUtil.ROOT_LOG.isDebugEnabled()) {
+			String[] activeProfiles = ctx.getEnvironment().getActiveProfiles();
+			if (A.isNotEmpty(activeProfiles)) {
+				LogUtil.ROOT_LOG.debug("current profile : ({})", A.toStr(activeProfiles));
+			}
+		}
+	}
+}
